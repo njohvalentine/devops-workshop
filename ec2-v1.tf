a5417e016dcb8a11184d7b2e10f71d274a384905
vally <vally@gmail.com>
2024-06-13 01:23:43 -0500

    adding vpc to our code

--- ec2-v1.tf
+++ ec2-v1.tf
@@ -8,15 +8,72 @@ resource "aws_instance" "demo-server" {
   instance_type = "t2.micro"
   key_name      = "terraformkey"
   security_groups = ["demo-sg"]
+  subnet_id = aws_subnet.demo_pub-subnet.id
   tags = {
     Name = "project_web-server"
   }
 
 }
 
+resource "aws_vpc" "demo-vpc" {
+    cidr_block = "10.5.0.0/16"
+    tags = {
+      Name = "demo-project-vpc"
+    }  
+}
+
+resource "aws_subnet" "demo_pub-subnet" {
+    vpc_id = aws_vpc.demo-vpc.id
+    cidr_block = "10.5.2.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1a"
+    tags = {
+      Name = "demo-project-public-subnet"
+    }
+}
+
+resource "aws_subnet" "demo_pub-subnet2" {
+    vpc_id = "aws_vpc.demo-vpc.id"
+    cidr_block = "10.5.3.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1a"
+    tags = {
+      Name = "demo-project-public-subnet2"
+    }
+}
+
+resource "aws_internet_gateway" "demo-IGW" {
+    vpc_id = "aws_vpc.demo-vpc.id"
+    tags = {
+      Name= "demo-projetc-IGW"
+    }
+}
+
+resource "aws_route_table" "demo-public-RT" {
+    vpc_id = "aws_vpc.demo-vpc.id"
+    route = {
+        cidr_block="0.0.0.0/0"
+        gateway_id=aws_internet_gateway.demo-IGW.id
+    }
+    tags = {
+      Name= "demo-project-public-RT"
+    }
+}
+
+resource "aws_route_table_association" "rt-asso" {
+    subnet_id = aws_subnet.demo_pub-subnet.id
+    route_table_id = aws_route_table.demo-public-RT.id
+}
+
+resource "aws_route_table_association" "rt-asso" {
+    subnet_id = aws_subnet.demo_pub-subnet2.id
+    route_table_id = aws_route_table.demo-public-RT.id
+}
+
 resource "aws_security_group" "demo-sg" {
   name        = "demo-sg"
   description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id = aws_vpc.demo-vpc.id
 
   tags = {
     Name = "demo-sg"
@@ -36,4 +93,6 @@ resource "aws_security_group" "demo-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
 
